53f9d3ba9c78f88ec19e3494c37b381a4d9c57ed
migmolrod <migmolrod@fastmail.es>
2024-10-12 20:08:49 +0200

    added implementation of the message listeners
    only a mockup, they will be actually implemented when we do saga
    pattern.

---
+++ order-service/order-domain/order-application-service/src/main/java/ovh/migmolrod/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -0,0 +1,24 @@
+package ovh.migmolrod.food.ordering.system.order.service.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import ovh.migmolrod.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import ovh.migmolrod.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
+
+@Slf4j
+@Validated
+@Service
+public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
+
+	@Override
+	public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+	}
+
+	@Override
+	public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+	}
+
+}
