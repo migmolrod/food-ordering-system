30de326451fb9f239fd822b34e9a72c28e7289e6
migmolrod <migmolrod@fastmail.es>
2024-11-01 21:16:25 +0100

    added spring boot starter class and config
    incremental #8
    
    includes all configs in order container module (order-service,
    kafka-config, kafka-producer-config and kafka-consumer-config
    properties.
    
    also typical configuration like port of the app (since this order
    container module will be the "executable" jar that will be actually
    deployed and run), logging and spring configurations (jpa, datasource)

---
+++ order-service/order-container/src/main/java/ovh/migmolrod/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package ovh.migmolrod.food.ordering.system.order.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "ovh.migmolrod.food.ordering.system.order.service.data")
+@EntityScan(basePackages = "ovh.migmolrod.food.ordering.system.order.service.data")
+@SpringBootApplication(scanBasePackages = "ovh.migmolrod.food.ordering.system")
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
