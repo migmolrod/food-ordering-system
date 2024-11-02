733e7aff002885abdb562a614e34fc1d0096c6d7
migmolrod <migmolrod@fastmail.es>
2024-11-02 13:38:10 +0100

    configured customer-service
    closes #10
    
    added schema and data initialization for customer service
    
    also fixed some errors in order-container (it was missing a dependency
    for order-application) and in order-data (order entity had a property
    called 'status', instead of 'orderStatus').

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb55', 'customer_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb56', 'customer_2', 'Second', 'User');
