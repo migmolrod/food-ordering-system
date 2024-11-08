bf9073dea1a66f6f74a2279420f420c6a1d7a71d
migmolrod <migmolrod@fastmail.es>
2024-11-08 20:32:30 +0100

    fix naming errors in init-data
    incremental #11
    incremental #16
    
    related to previous commit.
    some columns could not be found due to different naming between
    entities (java) and columns (sql).

--- payment-service/payment-container/src/main/resources/init-data.sql
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -1,7 +1,7 @@
 -- CUSTOMER 1
 INSERT INTO payment.credit_entries(id, customer_id, total_credit_amount)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'd215b5f8-0249-4dc5-89a3-51fd148cfb55', 500.00);
-INSERT INTO payment.credit_histories(id, customer_id, amount, type)
+INSERT INTO payment.credit_histories(id, customer_id, amount, transaction_type)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb43', 'd215b5f8-0249-4dc5-89a3-51fd148cfb55', 100.00, 'CREDIT'),
        ('d215b5f8-0249-4dc5-89a3-51fd148cfb44', 'd215b5f8-0249-4dc5-89a3-51fd148cfb55', 600.00, 'CREDIT'),
        ('d215b5f8-0249-4dc5-89a3-51fd148cfb45', 'd215b5f8-0249-4dc5-89a3-51fd148cfb55', 200.00, 'DEBIT');
@@ -9,5 +9,5 @@ VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb43', 'd215b5f8-0249-4dc5-89a3-51fd148
 -- CUSTOMER 2
 INSERT INTO payment.credit_entries(id, customer_id, total_credit_amount)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb42', 'd215b5f8-0249-4dc5-89a3-51fd148cfb56', 100.00);
-INSERT INTO payment.credit_histories(id, customer_id, amount, type)
+INSERT INTO payment.credit_histories(id, customer_id, amount, transaction_type)
 VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb46', 'd215b5f8-0249-4dc5-89a3-51fd148cfb56', 100.00, 'CREDIT');
