72fd73c44bbb07ad229f39fc66dd83a58435b6d6
migmolrod <migmolrod@fastmail.es>
2024-11-08 20:14:16 +0100

    implemented payment container module
    incremental #11
    incremental #16

---
+++ payment-service/payment-container/src/main/resources/init-schema.sql
@@ -0,0 +1,42 @@
+DROP SCHEMA IF EXISTS payment CASCADE;
+
+CREATE SCHEMA payment;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TYPE IF EXISTS payment_status;
+CREATE TYPE payment_status AS ENUM ('COMPLETED', 'CANCELLED', 'FAILED');
+
+DROP TABLE IF EXISTS payment.payments;
+CREATE TABLE payment.payments
+(
+    id UUID NOT NULL,
+    customer_id UUID NOT NULL,
+    order_id UUID NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    status payment_status NOT NULL,
+    CONSTRAINT pk_payment PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS payment.credit_entries;
+CREATE TABLE payment.credit_entries
+(
+    id UUID NOT NULL,
+    customer_id UUID NOT NULL,
+    total_credit_amount NUMERIC(10, 2) NOT NULL,
+    CONSTRAINT pk_credit_entry PRIMARY KEY (id)
+);
+
+DROP TYPE IF EXISTS transaction_type;
+CREATE TYPE transaction_type AS ENUM ('DEBIT', 'CREDIT');
+
+DROP TABLE IF EXISTS payment.credit_histories;
+CREATE TABLE payment.credit_histories
+(
+    id UUID NOT NULL,
+    customer_id UUID NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    type transaction_type NOT NULL,
+    CONSTRAINT pk_credit_history PRIMARY KEY (id)
+);
