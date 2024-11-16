10016b0ebfb930a1ad996f3f7ce884be19189058
migmolrod <migmolrod@fastmail.es>
2024-11-16 19:14:55 +0100

    created base saga module
    incremental #28
    
    the saga module just holds a saga step interface.
    also, a new empty event that does nothing has been added to common
    domain module, for saga steps that are ending operations.

---
+++ common/common-domain/src/main/java/ovh/migmolrod/food/ordering/system/domain/event/EmptyEvent.java
@@ -0,0 +1,12 @@
+package ovh.migmolrod.food.ordering.system.domain.event;
+
+public final class EmptyEvent implements DomainEvent<Void> {
+
+	public static final EmptyEvent INSTANCE = new EmptyEvent();
+
+	private EmptyEvent() {}
+
+	@Override
+	public void fire() {}
+
+}
