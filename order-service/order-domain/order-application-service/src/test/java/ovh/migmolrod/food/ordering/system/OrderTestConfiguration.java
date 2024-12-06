64a2a01245f656bf5f7e96c479dfec782b53c76e
migmolrod <migmolrod@fastmail.es>
2024-12-06 01:45:37 +0100

    refactor order app service tests
    incremental #31
    
    after outbox refactor, tests were broken. they still not pass because
    we need to implement output ports (adapters) in both modules (data and
    messaging).

--- order-service/order-domain/order-application-service/src/test/java/ovh/migmolrod/food/ordering/system/OrderTestConfiguration.java
+++ order-service/order-domain/order-application-service/src/test/java/ovh/migmolrod/food/ordering/system/OrderTestConfiguration.java
@@ -5,12 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ovh.migmolrod.food.ordering.system.order.service.domain.OrderDomainService;
 import ovh.migmolrod.food.ordering.system.order.service.domain.OrderDomainServiceImpl;
+import ovh.migmolrod.food.ordering.system.order.service.domain.ports.output.message.publisher.approval.ApprovalRequestMessagePublisher;
+import ovh.migmolrod.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.PaymentRequestMessagePublisher;
 import ovh.migmolrod.food.ordering.system.order.service.domain.ports.output.repository.CustomerRepository;
 import ovh.migmolrod.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
 import ovh.migmolrod.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
 
 @SpringBootApplication(scanBasePackages = "ovh.migmolrod.food.ordering.system")
 public class OrderTestConfiguration {
+
+	@Bean
+	public PaymentRequestMessagePublisher paymentRequestMessagePublisher() {
+		return Mockito.mock(PaymentRequestMessagePublisher.class);
+	}
+
+	@Bean
+	public ApprovalRequestMessagePublisher approvalRequestMessagePublisher() {
+		return Mockito.mock(ApprovalRequestMessagePublisher.class);
+	}
 
 	@Bean
 	public OrderRepository orderRepository() {
